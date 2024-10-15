617e808abc217b6922fce50f04095b0e61fae7fe
hajhi7899 <hajhi7899@gmail.com>
2024-10-16 00:25:09 +0900

    update 10/16

---
+++ src/test/java/com/lotteon/LotteOnProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotteon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteOnProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
