95aebf3011a50e37033c486c7bf95a65aa85bac4
jin123346 <hajhi7899@gmail.com>
2024-10-16 17:34:51 +0900

    update

---
+++ src/main/java/com/lotteon/controller/CsController.java
@@ -0,0 +1,21 @@
+package com.lotteon.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Log4j2
+@RequiredArgsConstructor
+@Controller
+public class CsController {
+
+
+
+    @GetMapping("/cs")
+    public String index(){
+        return "csIndex";
+    }
+
+}
