16a4563d261f92f68c97d0b0a9205f9d63f890db
jin123346 <hajhi7899@gmail.com>
2024-10-18 18:21:28 +0900

    update

--- src/main/java/com/lotteon/controller/AdminController.java
+++ src/main/java/com/lotteon/controller/AdminController.java
@@ -4,14 +4,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Log4j2
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
 
 
-    @GetMapping("/admin")
+    @GetMapping("/config/{}")
     public String index(){
         return "adminIndex";
     }
