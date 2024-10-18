c087968e5d3884b5e8b39e1a0397f5ef99ec8e11
jin123346 <hajhi7899@gmail.com>
2024-10-18 12:27:19 +0900

    update

---
+++ src/main/java/com/lotteon/controller/MarketController.java
@@ -0,0 +1,23 @@
+package com.lotteon.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Log4j2
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/market")
+public class MarketController {
+
+    @GetMapping("/{content}")
+    public String index(@PathVariable String content, Model model) {
+
+        model.addAttribute("content", content);
+        return "marketindex";
+    }
+}
