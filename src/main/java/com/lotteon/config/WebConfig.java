7d229209502f75a561225a101c751154743c53c4
dnjsrldus01 <dnjsrldus01@gmail.com>
2024-10-21 17:15:47 +0900

    feature/wgy1021

--- src/main/java/com/lotteon/config/WebConfig.java
+++ src/main/java/com/lotteon/config/WebConfig.java
@@ -1,5 +1,8 @@
 package com.lotteon.config;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -16,5 +19,18 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/css/**")
                 .addResourceLocations("classpath:/static/css/")
                 .setCachePeriod(0);
+    }
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        // DTO 와 Entity 간의 변환을 위한 ModelMapper 설정
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true);
+
+
+        return modelMapper;
     }
 }
