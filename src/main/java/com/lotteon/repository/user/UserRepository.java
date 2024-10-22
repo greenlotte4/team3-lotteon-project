48b67c55d280aa0b2df70ec05ac8f5f064852a93
lotte4 <dudwls4251@gamil.com>
2024-10-22 13:54:23 +0900

    update

--- src/main/java/com/lotteon/repository/user/UserRepository.java
+++ src/main/java/com/lotteon/repository/user/UserRepository.java
@@ -4,6 +4,11 @@ import com.lotteon.entity.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByUid(String uid);
+
 }
