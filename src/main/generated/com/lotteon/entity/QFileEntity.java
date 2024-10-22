59086d025a4517bb13f4ae5c3548bf7deac8dc09
sujeonq <sujeonq@gmail.com>
2024-10-22 17:07:07 +0900

    update CS css

---
+++ src/main/generated/com/lotteon/entity/QFileEntity.java
@@ -0,0 +1,51 @@
+package com.lotteon.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QFileEntity is a Querydsl query type for FileEntity
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QFileEntity extends EntityPathBase<FileEntity> {
+
+    private static final long serialVersionUID = 850073488L;
+
+    public static final QFileEntity fileEntity = new QFileEntity("fileEntity");
+
+    public final NumberPath<Integer> bno = createNumber("bno", Integer.class);
+
+    public final NumberPath<Integer> download = createNumber("download", Integer.class);
+
+    public final StringPath fileoption = createString("fileoption");
+
+    public final StringPath filetype = createString("filetype");
+
+    public final NumberPath<Integer> fno = createNumber("fno", Integer.class);
+
+    public final StringPath oName = createString("oName");
+
+    public final DateTimePath<java.time.LocalDateTime> rdate = createDateTime("rdate", java.time.LocalDateTime.class);
+
+    public final StringPath sName = createString("sName");
+
+    public QFileEntity(String variable) {
+        super(FileEntity.class, forVariable(variable));
+    }
+
+    public QFileEntity(Path<? extends FileEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QFileEntity(PathMetadata metadata) {
+        super(FileEntity.class, metadata);
+    }
+
+}
+
