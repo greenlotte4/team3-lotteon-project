837c1b11340bc4f777dea4a892c389d70b5af60c
jungmayo <wlgus5946@gmail.com>
2024-10-25 12:41:05 +0900

    페이징

--- src/main/java/com/lotteon/repository/custom/FaqRepositoryCustom.java
+++ src/main/java/com/lotteon/repository/custom/FaqRepositoryCustom.java
@@ -3,11 +3,11 @@ package com.lotteon.repository.custom;
 import com.lotteon.dto.page.PageRequestDTO;
 import com.querydsl.core.Tuple;
 import org.springframework.data.domain.Page;
-
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 
 public interface FaqRepositoryCustom {
 
     public Page<Tuple> selectFaqAllForList(PageRequestDTO pageRequestDTO, Pageable pageable);
+
     public Page<Tuple> selectFaqForSearch(PageRequestDTO pagerequestDTO, Pageable pageable);
 }
