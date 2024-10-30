f8524d114d1a9a296c90ccc055c2d251fe22c030
sujeonq <sujeonq@gmail.com>
2024-10-30 12:35:59 +0900

    update cs

--- src/main/java/com/lotteon/repository/admin/NoticeRepository.java
+++ src/main/java/com/lotteon/repository/admin/NoticeRepository.java
@@ -2,9 +2,12 @@ package com.lotteon.repository.admin;
 
 import com.lotteon.entity.Notice;
 import com.lotteon.repository.custom.NoticeRepositoryCustom;
+import org.springframework.data.domain.Page;  // 올바른 Page 클래스를 import
+import org.springframework.data.domain.Pageable; // Pageable도 올바르게 import
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NoticeRepository extends JpaRepository<Notice, Long> , NoticeRepositoryCustom {
+public interface NoticeRepository extends JpaRepository<Notice, Long>, NoticeRepositoryCustom {
+    Page<Notice> findAllByOrderByDateDesc(Pageable pageable); // 날짜 기준 내림차순 정렬
 }
