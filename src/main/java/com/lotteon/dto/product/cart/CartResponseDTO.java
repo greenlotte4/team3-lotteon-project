838bfb4613b482ebb72021ab334df41c07968a71
jin123346 <hajhi7899@gmail.com>
2024-11-06 14:49:49 +0900

    update cart

--- src/main/java/com/lotteon/dto/product/cart/CartResponseDTO.java
+++ src/main/java/com/lotteon/dto/product/cart/CartResponseDTO.java
@@ -1,25 +1,21 @@
-
 package com.lotteon.dto.product.cart;
 
 
 import com.lotteon.dto.product.OptionDTO;
 import com.lotteon.dto.product.OptionItemDTO;
 import com.lotteon.dto.product.ProductDTO;
-import com.lotteon.dto.product.ProductOptionCombinationDTO;
 import com.lotteon.entity.User.Member;
 import lombok.*;
 
-import java.util.Date;
 import java.util.List;
 
-@Setter
 @Getter
+@Setter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class CartItemDTO {
-    private long cartItemId;    // CartItem ID
+public class CartResponseDTO {
 
     private String productId;
     private String productName;
@@ -32,24 +28,17 @@ public class CartItemDTO {
     private String optionName;
     private String point;
     private String shippingFee;
-    private int deliveryFee;
-
     private String ShippingTerms;
     private int expectedPoint;
     private Member member;
     private List<OptionItemDTO> options;  // Use List<OptionDTO> for options
     private int stock;          // 재고
+    private Long cartItemId;    // CartItem ID
     private long price;         // 가격
     private List<OptionDTO> option;      // 옵션 (예: 색상, 사이즈 등)
     private ProductDTO productDTO; // 상품 정보
-    private ProductOptionCombinationDTO combinationDTO;
+
     private long total;         // 총 가격
-    private String imageUrl;
 
     private int points;         // 적립 포인트 (선택 사항)
-
-
-
 }
-
-
