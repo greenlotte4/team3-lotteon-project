77460375895c1dc99892970fcfbb79a7ea76a509
Dangi90 <woo24465@gmail.com>
2024-11-12 02:22:29 +0900

    oauth2 login

--- src/main/java/com/lotteon/dto/User/MemberDTO.java
+++ src/main/java/com/lotteon/dto/User/MemberDTO.java
@@ -1,18 +1,16 @@
 package com.lotteon.dto.User;
 
-
-import com.lotteon.entity.User.Member;
-import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import com.lotteon.entity.User.MemberStatus;
 
 @Builder
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor // 모든 필드를 초기화하는 생성자 추가
 @ToString
 public class MemberDTO {
 
@@ -25,33 +23,13 @@ public class MemberDTO {
     private String addr;        // Member의 addr
     private String addr2;       // Member의 addr2
     private BigDecimal point;   // Member의 point
-    private Grade grade;       // Member의 grade
+    private Grade grade;        // Member의 grade
+    private MemberStatus status; // Member의 status
     private String uid;         // User의 uid
-    private LocalDate regDate; // Member의 regDate
-    private LocalDate lastDate;
-
-
-    // 쿼리에서 사용하는 순서대로 생성자 작성
-    public MemberDTO(long id, String name, String gender, String email, String hp, String postcode,
-                     String addr, String addr2, BigDecimal point, Grade grade, String uid, LocalDate regDate, LocalDate lastDate) {
-        this.id = id;
-        this.name = name;
-        this.gender = gender;
-        this.email = email;
-        this.hp = hp;
-        this.postcode = postcode;
-        this.addr = addr;
-        this.addr2 = addr2;
-        this.point = point;
-        this.grade = grade;
-        this.uid = uid;
-        this.regDate = regDate;
-        this.lastDate = lastDate;
-    }
+    private LocalDate regDate;  // Member의 regDate
+    private LocalDate lastDate; // Member의 lastDate
 
     public Long getMemberId() {
         return this.id;
     }
 }
-
-
--- src/main/java/com/lotteon/oauth2/MyOauth2UserService.java
+++ src/main/java/com/lotteon/oauth2/MyOauth2UserService.java
@@ -1,147 +1,270 @@
-//package com.lotteon.oauth2;
-//
-//
-//import com.lotteon.entity.User.User;
-//import com.lotteon.entity.User.Member;
-//import com.lotteon.repository.user.UserRepository;
-//import com.lotteon.repository.user.MemberRepository;
-//import com.lotteon.security.MyUserDetails;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.log4j.Log4j2;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-//import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-//import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-//import org.springframework.security.oauth2.core.user.OAuth2User;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Map;
-//import java.util.Optional;
-//
-//@Service
-//@Log4j2
-//@RequiredArgsConstructor
-//public class MyOauth2UserService extends DefaultOAuth2UserService {
-//
-//
-//    private final UserRepository userRepository;
-//
-//
-//    @Override
-//    public OAuth2User loadUser(OAuth2UserRequest userRequest) {
-//        log.info("loadUser..1 : "+userRequest);
-//
-//        String accessToken = userRequest.getAccessToken().getTokenValue();
-//        log.info("loadUser..2 accessToken : "+accessToken);
-//
-//        String provider= userRequest.getClientRegistration().getRegistrationId();
-//        log.info("loadUser..3 provider : "+provider);
-//
-//
-//        String uid=null;
-//        String name =null;
-//        String email=null;
-//        String nick=null;
-//        String hp=null;
-//        Map<String, Object> attributes;
-//
-//        if ("google".equals(provider)) {
-//            OAuth2User oAuth2User = super.loadUser(userRequest);
-//            log.info("loadUser..4 oAuth2User : "+oAuth2User);
-//
-//
-//            attributes  = oAuth2User.getAttributes();
-//            log.info("loadUser..5 attributes : "+attributes);
-//
-//            // 제공자별 사용자 정보 처리
-//
-//            email = (String) attributes.get("email");
-//            uid = email.split("@")[0];
-//            name = (String) attributes.get("given_name");
-//            nick = (String) attributes.get("name");
-//        } else if ("kakao".equals(provider)) {
-//            OAuth2User oAuth2User = super.loadUser(userRequest);
-//            log.info("loadUser..4 oAuth2User : "+oAuth2User);
-//
-//
-//            attributes  = oAuth2User.getAttributes();
-//            log.info("loadUser..5 attributes : "+attributes);
-//            String id = attributes.get("id").toString();
-//
-//            // 카카오 사용자 정보에서 properties 가져오기
-//            Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
-//            log.info("loadUser..6 properties : "+properties);
-//
-//            Map<String, Object> kakao_account= (Map<String, Object>) attributes.get("kakao_account");
-//            log.info("loadUser..7 kakao_account : "+kakao_account);
-//
-//            Map<String, Object> profile = (Map<String, Object>) kakao_account.get("profile");
-//            log.info("loadUser..8 profile : "+profile);
-//
-//            email = (String) kakao_account.get("email");
-//            log.info("loadUser..9 email : "+email);
-//            uid= email.split("@")[0];
-//            name = (String) properties.get("nickname");
-//            nick = name+id.substring(0,4);
-//        }else if("naver".equals(provider)){
-//            OAuth2User oAuth2User = super.loadUser(userRequest);
-//            log.info("loadUser..4 oAuth2User : "+oAuth2User);
-//
-//
-//            attributes = oAuth2User.getAttributes();
-//            Map<String, Object> response = (Map<String, Object>) attributes.get("response");
-//            log.info("loadUser..5 attributes : "+attributes);
-//
-//
-//            email = (String) response.get("email");
-//            uid ="N"+ email.split("@")[0];
-//            name = (String) response.get("name");
-//            nick = (String) response.get("nickname");
-//            hp = (String) response.get("hp");
-//            log.info("naver : hp "+hp);
-//
-//        }else {
-//            throw new OAuth2AuthenticationException("Unsupported provider: " + provider);
-//        }
-//
-//        Optional<User> opt= userRepository.findById(uid);
-//
-//        if(opt.isPresent()){
-//
-//            //회원이 존재하면
-//            User user = opt.get();
-//            log.info("loadUser..7 user : "+user);
-//
-//            if(user.getRole().equals("BLACK") || user.getRole().equals("LEAVE")){
-//                throw new UsernameNotFoundException("User does not have the required role.");
-//            }
-//            return MyUserDetails.builder()
-//                    .user(user)
-////                    .accessToken(accessToken)
-//                    .attributes(attributes)
-//                    .build();
-//        }else{
-//            //회원이 존재하지 않으면 회원가입처리
-//
-//
-//            User user = User.builder()
-//                    .uid(uid)
-//                    .name(name)
-//                    .role("USER")
-//                    .email(email)
-//                    .nick(nick)
-//                    .hp(hp)
-//                    .build();
-//
-//            userRepository.save(user);
-//
-//            return MyUserDetails.builder()
-//                    .user(user)
-//                    .accessToken(accessToken)
-//                    .attributes(attributes)
-//                    .build();
-//        }
-//
-//
-//    }
-//}
+package com.lotteon.oauth2;
+
+
+import com.lotteon.dto.User.Grade;
+import com.lotteon.dto.User.MemberDTO;
+import com.lotteon.entity.User.MemberStatus;
+import com.lotteon.entity.User.User;
+import com.lotteon.entity.User.Member;
+import com.lotteon.repository.user.UserRepository;
+import com.lotteon.repository.user.MemberRepository;
+import com.lotteon.security.MyUserDetails;
+import com.lotteon.service.user.MemberService;
+import com.lotteon.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class MyOauth2UserService extends DefaultOAuth2UserService {
+
+
+    private final UserRepository userRepository;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        if (userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+
+            log.info("loadUser..1 : " + userRequest);
+
+            String accessToken = userRequest.getAccessToken().getTokenValue();
+            log.info("loadUser..2 accessToken : " + accessToken);
+
+            String provider = userRequest.getClientRegistration().getRegistrationId();
+            log.info("loadUser..3 provider : " + provider);
+
+            OAuth2User oAuth2User = super.loadUser(userRequest);
+            log.info("loadUser...4 : " + oAuth2User);
+
+            Map<String, Object> attributes = oAuth2User.getAttributes();
+            log.info("loadUser...5 : " + attributes);
+
+            // 사용자 확인 및 회원가입 처리
+            String email = (String) attributes.get("email");
+            String uid = "GOOGLE_" + email.split("@")[0];
+            String name = attributes.get("given_name").toString();
+
+            Optional<User> optUser = userRepository.findByUid(uid);
+
+            if (optUser.isPresent()) {
+                // 회원 존재하면 시큐리티 인증처리(로그인)
+                User user = optUser.get();
+
+                return MyUserDetails.builder()
+                        .user(user)
+                        .member(user.getMember())
+                        .attributes(attributes)
+                        .build();
+            } else {
+                // 신규 회원일 경우
+                MemberDTO memberDTO = MemberDTO.builder()
+                        .name(name)
+                        .email(email)
+                        .regDate(LocalDate.now()) // LocalDate 사용
+                        .grade(Grade.FAMILY) // 기본값 설정
+                        .status(MemberStatus.ACTIVE)
+                        .build();
+
+                Member member = Member.builder()
+                        .name(memberDTO.getName())
+                        .email(memberDTO.getEmail())
+                        .regDate(memberDTO.getRegDate().atStartOfDay()) // LocalDate -> LocalDateTime 변환
+                        .grade(memberDTO.getGrade())
+                        .status(memberDTO.getStatus())
+                        .build();
+
+
+                memberRepository.save(member);
+
+
+                User user = User.builder()
+                        .uid(uid)
+                        .member(member) // Member 엔티티 연결
+                        .role(User.Role.valueOf("MEMBER")) // 기본 역할 설정
+                        .build();
+
+                userRepository.save(user);
+
+                return MyUserDetails.builder()
+                        .user(user)
+                        .member(user.getMember())
+                        .attributes(attributes)
+                        .build();
+            }
+
+
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("kakao")) {
+            log.info("Kakao 로그인...1 : " + userRequest);
+
+            String accessToken = userRequest.getAccessToken().getTokenValue();
+            log.info("Kakao 로그인...2 : " + accessToken);
+
+            OAuth2User oAuth2User = super.loadUser(userRequest);
+            log.info("Kakao 로그인...3 : " + oAuth2User);
+
+            Map<String, Object> attributes = oAuth2User.getAttributes();
+            log.info("Kakao 로그인...4 : " + attributes);
+
+            // 카카오 응답에서 사용자 정보 가져오기
+            Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+            Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+
+            // 이메일 정보 추출
+            String email = (String) kakaoAccount.get("email");
+
+            // 닉네임 또는 이름 정보 추출
+            String name = (String) profile.get("nickname");
+
+            // 이메일에서 사용자 uid 추출
+            String uid = email != null ? "KAKAO_" + email.split("@")[0] : null;
+
+            // 결과 출력
+            System.out.println("이메일: " + email);
+            System.out.println("이름: " + name);
+            System.out.println("사용자 ID: " + uid);
+
+
+            Optional<User> optUser = userRepository.findByUid(uid);
+
+            if (optUser.isPresent()) {
+                // 회원 존재하면 시큐리티 인증처리(로그인)
+                User user = optUser.get();
+
+                return MyUserDetails.builder()
+                        .user(user)
+                        .member(user.getMember())
+                        .attributes(attributes)
+                        .build();
+            } else {
+                // 신규 회원일 경우
+                MemberDTO memberDTO = MemberDTO.builder()
+                        .name(name)
+                        .email(email)
+                        .regDate(LocalDate.now()) // LocalDate 사용
+                        .grade(Grade.FAMILY) // 기본값 설정
+                        .status(MemberStatus.ACTIVE)
+                        .build();
+
+                Member member = Member.builder()
+                        .name(memberDTO.getName())
+                        .email(memberDTO.getEmail())
+                        .regDate(memberDTO.getRegDate().atStartOfDay()) // LocalDate -> LocalDateTime 변환
+                        .grade(memberDTO.getGrade())
+                        .status(memberDTO.getStatus())
+                        .build();
+
+
+                memberRepository.save(member);
+
+
+                User user = User.builder()
+                        .uid(uid)
+                        .member(member) // Member 엔티티 연결
+                        .role(User.Role.valueOf("MEMBER")) // 기본 역할 설정
+                        .build();
+
+                userRepository.save(user);
+
+                return MyUserDetails.builder()
+                        .user(user)
+                        .member(user.getMember())
+                        .attributes(attributes)
+                        .build();
+            }
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+            log.info("Naver 로그인...1 : " + userRequest);
+
+            String accessToken = userRequest.getAccessToken().getTokenValue();
+            log.info("Naver 로그인...2 : " + accessToken);
+
+            OAuth2User oAuth2User = super.loadUser(userRequest);
+            log.info("Naver 로그인...3 : " + oAuth2User);
+
+            Map<String, Object> attributes = oAuth2User.getAttributes();
+            log.info("Naver 로그인...4 : " + attributes);
+
+            // 카카오 응답에서 사용자 정보 가져오기
+            Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+            // 이메일 정보 추출
+            String email = (String) response.get("email");
+
+            // 닉네임 또는 이름 정보 추출
+            String name = (String) response.get("name");
+
+            // 이메일에서 사용자 uid 추출
+            String uid = email != null ? "NAVER_" + email.split("@")[0] : null;
+
+            // 결과 출력
+            System.out.println("이메일: " + email);
+            System.out.println("이름: " + name);
+            System.out.println("사용자 ID: " + uid);
+
+            Optional<User> optUser = userRepository.findByUid(uid);
+
+            if (optUser.isPresent()) {
+                // 회원 존재하면 시큐리티 인증처리(로그인)
+                User user = optUser.get();
+
+                return MyUserDetails.builder()
+                        .user(user)
+                        .member(user.getMember())
+                        .attributes(attributes)
+                        .build();
+            } else {
+                // 신규 회원일 경우
+                MemberDTO memberDTO = MemberDTO.builder()
+                        .name(name)
+                        .email(email)
+//                        .gender(gender)
+//                        .hp(hp)
+                        .regDate(LocalDate.now()) // LocalDate 사용
+                        .grade(Grade.FAMILY) // 기본값 설정
+                        .status(MemberStatus.ACTIVE)
+                        .build();
+
+                Member member = Member.builder()
+                        .name(memberDTO.getName())
+                        .email(memberDTO.getEmail())
+                        .regDate(memberDTO.getRegDate().atStartOfDay()) // LocalDate -> LocalDateTime 변환
+                        .grade(memberDTO.getGrade())
+                        .status(memberDTO.getStatus())
+                        .build();
+
+
+                memberRepository.save(member);
+
+
+                User user = User.builder()
+                        .uid(uid)
+                        .member(member) // Member 엔티티 연결
+                        .role(User.Role.valueOf("MEMBER")) // 기본 역할 설정
+                        .build();
+
+                userRepository.save(user);
+
+                return MyUserDetails.builder()
+                        .user(user)
+                        .member(user.getMember())
+                        .attributes(attributes)
+                        .build();
+            }
+
+        }
+        return null;
+    }
+}
